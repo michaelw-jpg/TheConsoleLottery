df41371319bf8ab89dc7cb86faf8140dd5565b05
micha <micha@MYPC>
2024-09-18 12:08:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TheConsoleLottery
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            "main menu"
+            switch 1:
+                buytickets()
+                break;
+            switch 2:
+                startgame()
+                break;
+            switch 3:
+                settings()
+                break;
+            switch 4:
+                exit
+                break;
+
+
+        }
+    }
+}
